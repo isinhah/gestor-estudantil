2bcc299890616b662ffd5e10c6a2264cf1f8749c
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-03-19 16:43:41 -0300

    criação das entidades Student e Major

---
+++ src/model/entities/Major.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Major {
+    private int id;
+    private String name;
+
+    public Major() {}
+
+    public Major(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Major major = (Major) o;
+        return id == major.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Major{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/model/entities/Student.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+
+    public Student() {}
+
+    public Student(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
